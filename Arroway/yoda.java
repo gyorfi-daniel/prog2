5d3719c403ed2d6eca0314f31275053a2f51b27a
gyorfi-daniel <43523045+gyorfi-daniel@users.noreply.github.com>
2019-09-25 00:41:21 +0200

    Create yoda.java

---
+++ Arroway/yoda.java
@@ -0,0 +1,15 @@
+public class yoda {
+	public static void main(String[] args){
+	String s=null;
+	if 
+		/*(s.equals("yoda"))*/ //Ebben az esetben áll le NullPinterException-nal
+		("yoda".equals(s))     //Lefordul, de nem fut le, nem írja ki, hogy Yoda
+		
+		{
+	
+		System.out.println("Yoda");
+	
+	
+	}
+	}
+}
