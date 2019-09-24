ffea14d68565af104e319bde259747082b61f4d6
gyorfi-daniel <43523045+gyorfi-daniel@users.noreply.github.com>
2019-09-24 22:40:17 +0200

    Create gagyi1.java

---
+++ Arroway/gagyi1.java
@@ -0,0 +1,13 @@
+public class gagyi1
+{
+
+public static void main (String[]args)
+{
+
+Integer x = -129;
+Integer t = -129;
+ System.out.println (x);
+ System.out.println (t);
+ 
+ while (x<=t && x>=t && t!=x);
+ } 
